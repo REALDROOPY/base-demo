164351ef355248fd5974bd90d344a11fb0a0d0ed
jiangkun <jiangkun@chexiang.com>
2018-08-16 14:02:45 +0800

    init

---
+++ src/main/java/com/example/framework/demoparent/base/entity/BaseEntity.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C), 2013-2018, 上汽集团
+ * FileName: BaseEntity.java
+ * Author:   jiangkun
+ * Date:     2018/8/14 10:59
+ * Description:
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package com.example.framework.demoparent.base.entity;
+
+import java.util.Date;
+
+/**
+ * 〈一句话功能简述〉<br>
+ *
+ * @author jiangkun
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class BaseEntity implements java.io.Serializable {
+
+    private Long id;
+    private Long createBy;
+    private Date createTime;
+    private Long updateBy;
+    private Date updateTime;
+
+    public BaseEntity() {
+        super();
+    }
+
+    public BaseEntity(Long id) {
+        this();
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("BaseEntity{");
+        sb.append("id=").append(id);
+        sb.append(", createBy=").append(createBy);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateBy=").append(updateBy);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Long createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(Long updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
